ecc175f31c4e798d87c5b51b13b83d67819adfbe
Rohan Gosavi <Rohan8999@users.noreply.github.com>
2023-06-02 09:59:25 +0530

    update version 1.2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ window.addEventListener("scroll", function() {
 });
 
 
-
 // preloader
 $(window).on("load", function () {
   $(".preloader").fadeOut("slow");
@@ -25,6 +24,37 @@ function scrollFunction() {
     document.getElementById("scrollToTopButton").style.display = "none";
   }
 }
+
+
+//custom js Menu
+
+(function($) { 
+  $(function() { 
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
+
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -55,39 +85,7 @@ function reveal() {
     }
   }
 }
-
 window.addEventListener("scroll", reveal);
-
-
-
-
-//custom js Menu
-(function ($) {
-  $(function () {
-    // If a link has a dropdown, add sub menu toggle.
-    $("nav ul li a:not(:only-child)").click(function (e) {
-      $(this).siblings(".nav-dropdown").toggle();
-      // Close one dropdown when selecting another
-      $(".nav-dropdown").not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-
-    // Clicking away from dropdown will remove the dropdown class
-    $("html").click(function () {
-      $(".nav-dropdown").hide();
-    });
-    // Toggle open and close nav styles on click
-
-    $("#nav-toggle").click(function () {
-      $("nav ul").slideToggle();
-    });
-
-    // Hamburger to X toggle
-    $("#nav-toggle").on("click", function () {
-      this.classList.toggle("active");
-    });
-  }); // end DOM ready
-})(jQuery); // end jQuery
 
 
 
